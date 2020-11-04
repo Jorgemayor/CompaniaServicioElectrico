866a2e40ba92d3a7058560a1098f5a2e13f9f69f
cesar becerra <sezaru@hotmail.es>
2020-11-03 23:17:10 -0500

    Funciones obtener, agregar y cambiar estado terminadas

--- src/managers/gestionClientes/GestionClienteApi.java
+++ src/managers/gestionClientes/GestionClienteApi.java
@@ -1,27 +1,105 @@
 package src.managers.gestionClientes;
 
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class GestionClienteApi {
     
     private static GestionClienteLib lib = new GestionClienteLib();
 
-    public static String obtenerCliente(){
-        return "";
+    public static String obtenerClientes(){
+        String resultado = "";
+        try {
+            resultado = lib.obtenerClientes();
+        } catch(SQLException excepcion) {
+
+            return retornarError("-1");
+        }
+
+        return resultado;
     }
-    public static String agregarCliente(){
-        return "";
+    public static String agregarCliente(String tipoIdentificacion, int identificacion, String nombre, String direccion, int ciudad){
+
+        String resultado = "";
+
+        try {
+            if(tipoIdentificacion.trim().equals("")) {
+                throw new Exception("-2");
+            } else if(nombre.trim().equals("")) {
+                throw new Exception("-6");
+            } else if(direccion.trim().equals("")) {
+                throw new Exception("-3");
+            } else if(ciudad < 0) {
+                throw new Exception("-4");
+            } else {
+                resultado = lib.agregarCliente(tipoIdentificacion, identificacion, nombre, direccion, ciudad);
+            }
+
+            if(!resultado.equals("0")) {
+                throw new Exception(resultado);
+            }
+        } catch(SQLException excepcion) {
+            return retornarError("-1");
+        } catch(Exception excepcion) {
+            return retornarError(excepcion.getMessage());
+        }
+        return resultado;
     }
-    public static String actualizarCliente(){
-        return "";
+
+
+    public static boolean actualizarCliente(int idCliente){
+        return true;
     }
-    public static String cambiarEstadoCliente(){
-        return "";
+    public static String cambiarEstadoCliente(int idCliente){
+        String resultado = "";
+
+        try {
+            if(idCliente < 0) {
+                throw new Exception("-5");
+            } else {
+                resultado = lib.cambiarEstadoCliente(idCliente);
+            }
+        } catch(SQLException excepcion) {
+            return retornarError("-1");
+        } catch(Exception excepcion) {
+            return retornarError(excepcion.getMessage());
+        }
+        return resultado;
     }
-    public static String retornarError(){
-        return "";
+    public static String retornarError(String codigoExcepcion){
+        String mensajeError = "{ \"code\": ";
+        int codigo;
+        
+        try {
+            codigo = Integer.parseInt(codigoExcepcion);
+        } catch (NumberFormatException excepcion) {
+            return "Error inesperado: " + codigoExcepcion;
+        }
+
+        mensajeError += codigoExcepcion + ",  \"mensaje\": ";
+
+        switch(codigo) {
+            case -1:
+                mensajeError += "Error al conectarse a la base de datos}";
+                break;
+            case -2:
+                mensajeError += "El Tipo de Identificaicón no puede ser vacio}";
+                break;
+            case -3:
+                mensajeError += "La Direccion no puede ser vacio}";
+            case -4:
+                mensajeError += "Ciudad invalida}";
+                break;
+            case -5:
+                mensajeError += "No existe un Cliente con ese ID}";
+                break;
+            case -6:
+                mensajeError += "Nombre del cliente no puede ser vació}";
+                break;
+            default:
+                mensajeError += "El código de error " + codigo + " no ha sido identificado}";
+                break;
+        }
+        return mensajeError;
     }
 
 
--- src/managers/gestionClientes/GestionClienteLib.java
+++ src/managers/gestionClientes/GestionClienteLib.java
@@ -1,21 +1,74 @@
 package src.managers.gestionClientes;
 
+import java.sql.*;
+import src.control.Conexion;
+
 public class GestionClienteLib {
+
+    // variables para encriptacion
+    private static final String KEY = "gn2byqYnYFlJMzG5";
+    private static final String IV = "HFNvUwjB1KiOKtJI";
     
 
-    public String agregarCliente(String tipoIdentificacion, int identificacion, String nombre, String direccion) {
+    public String agregarCliente(String tipoIdentificacion, int identificacion, String nombre, String direccion, int ciudad) throws SQLException {
 
-        //Insersion
+        Connection conexion = Conexion.conectar();
+        String respuesta = "";
 
-        return "";
+        String consultaSQL = "SELECT * FROM cliente WHERE habilitado = ?";
+        PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
+        consulta.setString(1, nombre);
+        ResultSet resultadoConsulta = consulta.executeQuery();
+
+        if(resultadoConsulta.next()) {
+            respuesta = "-8";
+        } else {
+            String insercionSQL = "INSERT INTO cliente(tipo_identificacion=?,identificacion=?,nombre=?,direccion=?,ciudad=?)";
+            PreparedStatement insercion = conexion.prepareStatement(insercionSQL);
+            insercion.setString(1, tipoIdentificacion);
+            insercion.setInt(2, identificacion);
+            insercion.setString(3, nombre);
+            insercion.setString(4, direccion);
+            insercion.setInt(5, ciudad);
+            insercion.executeQuery();
+
+            respuesta = "0";
+        }
+
+        conexion.close();
+        return respuesta;
     }
-    public String obtenerCliente(){
-        return "";
+    public String obtenerClientes() throws SQLException {
+        Connection conexion = Conexion.conectar();
+
+        String consultaSQL = "SELECT * FROM cliente WHERE habilitado = ?";
+
+        PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
+        consulta.setBoolean(1, true);
+        ResultSet respuesta = consulta.executeQuery();
+
+        conexion.close();
+
+        return respuesta.toString();
     }
     public String actualizarCliente(){
         return "";
     }
-    public String cambiarEstadoCliente(){
-        return "";
+    public String cambiarEstadoCliente(int idCliente) throws SQLException {
+        Connection conexion = Conexion.conectar();
+        String consultaSQL = "SELECT habilitado FROM cliente WHERE id = ?";
+        PreparedStatement consultaEstado = conexion.prepareStatement(consultaSQL);
+        consultaEstado.setInt(1, idCliente);
+        ResultSet estadoActual = consultaEstado.executeQuery();
+        
+        if(estadoActual.next()) {
+            String actualizacionSQL = "UPDATE cliente SET habilitado = ? WHERE id = ?";
+            PreparedStatement actualizarEstado = conexion.prepareStatement(actualizacionSQL);
+            actualizarEstado.setBoolean(1, !estadoActual.getBoolean("habilitado"));
+            actualizarEstado.setInt(2, idCliente);
+            actualizarEstado.executeQuery();
+        }
+        conexion.close();
+        return "{\"code\": 0, \"result\": " + "true" +"}";
     }
 }
