2fad617981f6cf08b508091b3275c758e522832a
Jorgemayor <jorge.e.mayor@gmail.com>
2020-11-15 23:40:16 -0500

    Creadas vistas basicas

--- src/vista/DeshabilitarUsuario.java
+++ src/vista/DeshabilitarUsuario.java
@@ -6,9 +6,9 @@ import java.awt.BorderLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
-public class Inicio extends Container {
+public class DeshabilitarUsuario extends Container {
 
-    public Inicio() {
+    public DeshabilitarUsuario(){
         JLabel imagenBienvenida = new JLabel();
         imagenBienvenida.setIcon(new ImageIcon("src/assets/inicio.png"));
         imagenBienvenida.setVisible(true);
