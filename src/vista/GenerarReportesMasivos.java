0bd70cba6a219976988cdc2b1ce8a5f99c114460
Carlos Mauricio Tovar Parra <carlos.mauricio.tovar@correounivalle.edu.co>
2020-11-22 01:02:07 -0500

    añadidas todas las vistas faltantes

--- src/vista/GenerarReportesMasivos.java
+++ src/vista/GenerarReportesMasivos.java
@@ -2,16 +2,49 @@ package src.vista;
 
 import java.awt.Container;
 import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Color;
 
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 
 public class GenerarReportesMasivos extends Container {
+ 
+    private static final Font FUENTE_TITULO = new Font(null, Font.BOLD, 28);
+    private static final Font FUENTE_ETIQUETAS = new Font(null, Font.BOLD, 22);
+    private static final Color COLOR_FONDO = new Color(232,234,246);
 
-    public GenerarReportesMasivos() {
-        JLabel imagenBienvenida = new JLabel();
-        imagenBienvenida.setIcon(new ImageIcon("src/assets/inicio.png"));
-        imagenBienvenida.setVisible(true);
-        this.add(imagenBienvenida, BorderLayout.CENTER);
+    private JPanel contenido;
+    private JLabel titulo;
+    private JPanel reporte;
+
+    public GenerarReportesMasivos(){
+        iniciarComponentes();
     }
+    
+    private void iniciarComponentes() {
+
+        contenido = new JPanel();
+        titulo = new JLabel("Reporte General de Clientes");
+        reporte = new JPanel();
+
+        contenido.setLayout(null);
+        contenido.setVisible(true);
+        contenido.setBackground(COLOR_FONDO);
+
+        //Titulo
+        titulo.setFont(FUENTE_TITULO);
+        titulo.setVisible(true);
+        titulo.setBounds(450, 30, 400, 25);
+        contenido.add(titulo);
+
+        
+
+        //Contenedor Reporte
+        reporte.setVisible(true);
+        reporte.setBounds(150,100,950,450);
+        contenido.add(reporte);
+        this.add(contenido, BorderLayout.CENTER);
+    }  
 }
