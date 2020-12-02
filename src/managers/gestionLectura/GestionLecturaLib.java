dd54b7e33062aa91a0af81cf1767ca2a7a76637c
Jorgemayor <jorge.e.mayor@gmail.com>
2020-12-02 05:24:42 -0500

    Corregida funcionalidad insertar lectura

--- src/managers/gestionLectura/GestionLecturaLib.java
+++ src/managers/gestionLectura/GestionLecturaLib.java
@@ -12,8 +12,8 @@ import src.control.Conexion;
 public class GestionLecturaLib {
 
     public String obtenerLecturasPorCliente(int identificacionCliente) throws SQLException {
+        
         Connection conexion = Conexion.conectar();
-
         String consultaSQL = ""
             + "SELECT *"
             + "FROM lectura AS L INNER JOIN cliente AS C ON L.id_cliente = C.id"
@@ -22,12 +22,12 @@ public class GestionLecturaLib {
         PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
         consulta.setInt(1, identificacionCliente);
         consulta.setBoolean(2, true);
-        ResultSet respuesta = consulta.executeQuery();
+        ResultSet respuestaConsulta = consulta.executeQuery();
         JSONObject resultado = new JSONObject();
 
-        while(respuesta.next()) {
-            resultado.append("fecha", respuesta.getString(1));
-            resultado.append("consumo", respuesta.getString(2));
+        while(respuestaConsulta.next()) {
+            resultado.append("fecha", respuestaConsulta.getString(1));
+            resultado.append("consumo", respuestaConsulta.getString(2));
         }
 
         conexion.close();
@@ -35,8 +35,8 @@ public class GestionLecturaLib {
     }
 
     public String obtenerUltimaLecturaPorCliente(int identificacionCliente) throws SQLException {
+        
         Connection conexion = Conexion.conectar();
-
         String consultaSQL = ""
             + "SELECT S.fecha, L.consumo" 
             + "FROM (SELECT L.id_cliente as LIDC, MAX(L.fecha) AS fecha"
@@ -47,32 +47,34 @@ public class GestionLecturaLib {
         PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
         consulta.setInt(1, identificacionCliente);
         consulta.setBoolean(2, true);
-        ResultSet respuesta = consulta.executeQuery();
+        ResultSet respuestaConsulta = consulta.executeQuery();
         JSONObject resultado = new JSONObject();
 
-        if(respuesta.next()) {
-            resultado.append("fecha", respuesta.getString(1));
-            resultado.append("consumo", respuesta.getString(2));
+        if(respuestaConsulta.next()) {
+            resultado.append("fecha", respuestaConsulta.getString(1));
+            resultado.append("consumo", respuestaConsulta.getString(2));
         }
 
         conexion.close();
         return resultado.toString();
     }
 
-    public String ingresarLectura(int idCliente, String fecha, int consumo) throws SQLException {
+    public String ingresarLectura(int identificacionCliente, String fecha, int consumo) throws SQLException {
 
         Connection conexion = Conexion.conectar();
-        String respuesta = "";
+        String resultado = "";
 
-        String consultaSQL = "SELECT * FROM cliente WHERE id=? AND habilitado=?";
+        String consultaSQL = "SELECT id FROM cliente WHERE identificacion=? AND habilitado=?";
         PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
-        consulta.setInt(1, idCliente);
+        consulta.setInt(1, identificacionCliente);
         consulta.setBoolean(2, true);
-        ResultSet resultadoConsulta = consulta.executeQuery();
+        ResultSet respuestaConsulta = consulta.executeQuery();
 
-        if(resultadoConsulta.next()) {
-            respuesta = "-7";
+        if(!respuestaConsulta.next()) {
+            resultado = "-7";
         } else {
+
+            int idCliente = respuestaConsulta.getInt(1);
             String insercionSQL = "INSERT INTO lectura (id_cliente, fecha, consumo) VALUES (?, ?, ?)";
             PreparedStatement insercion = conexion.prepareStatement(insercionSQL);
             insercion.setInt(1, idCliente);
@@ -80,10 +82,10 @@ public class GestionLecturaLib {
             insercion.setInt(3, consumo);
             insercion.executeUpdate();
             
-            respuesta = "0";
+            resultado = "0";
         }
 
         conexion.close();
-        return respuesta;
+        return resultado;
     }
 }
