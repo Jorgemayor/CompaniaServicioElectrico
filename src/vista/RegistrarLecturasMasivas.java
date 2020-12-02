97fabca013e3a57a5d5d47f58a618d9bbf18eba8
Carlos Mauricio Tovar Parra <carlos.mauricio.tovar@correounivalle.edu.co>
2020-12-01 19:51:13 -0500

    añadido esquema para vistas de registro de lecturas

---
+++ src/vista/RegistrarLecturasMasivas.java
@@ -0,0 +1,27 @@
+package src.vista;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.BorderLayout;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class RegistrarLecturasMasivas extends Container {
+
+    private JLabel imagenBienvenida;
+    private ImageIcon laImagen;
+    private Icon imagenAjustada;
+
+    public RegistrarLecturasMasivas() {
+        imagenBienvenida = new JLabel();
+        imagenBienvenida.setSize(new Dimension(1280, 575));
+        laImagen = new ImageIcon("src/assets/inicio.png");
+        imagenAjustada = new ImageIcon(laImagen.getImage().getScaledInstance(imagenBienvenida.getWidth(),imagenBienvenida.getHeight(),Image.SCALE_DEFAULT));
+        imagenBienvenida.setIcon(imagenAjustada);
+        imagenBienvenida.setVisible(true);
+        this.add(imagenBienvenida, BorderLayout.CENTER);
+    }
+}
