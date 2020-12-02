289ef34ba72cbc8c23ec146617640aadf03099a0
cesar becerra <sezaru@hotmail.es>
2020-12-02 05:08:24 -0500

    avances en reporte individual

--- src/vista/GenerarReporteIndividual.java
+++ src/vista/GenerarReporteIndividual.java
@@ -28,7 +28,6 @@ public class GenerarReporteIndividual extends Container {
     private static final Color COLOR_FONDO = new Color(232,234,246);
 
     private JPanel panelContenido;
-    private JLabel etiquetaTitulo;
     private JLabel etiquetaId;
     private JTextField campoId;
     private JButton botonGenerar;
@@ -49,38 +48,33 @@ public class GenerarReporteIndividual extends Container {
         datos = new DefaultCategoryDataset();
         Grafica = ChartFactory.createLineChart("CONSUMO MENSUAL","Días", "Visitas", datos,PlotOrientation.VERTICAL, true, true, false);
         panel = new ChartPanel(Grafica);
+        panelDelPanel = new JPanel();
+        panelDelPanel.add(panel);
         panelContenido = new JPanel();
-        panelContenido.add(panel);
-        etiquetaTitulo = new JLabel("Generar Reporte Individual");
         etiquetaId = new JLabel("ID Cliente");
         campoId = new JTextField();
-        botonGenerar = new JButton("Generar Reporte de Cliente");
-        reporte = new JScrollPane(panelContenido);
+        botonGenerar = new JButton("Generar");
+        botonLimpiar = new JButton("Limpiar");
+        reporte = new JScrollPane(panelDelPanel);
 
         panelContenido.setLayout(null);
         panelContenido.setVisible(true);
         panelContenido.setBackground(COLOR_FONDO);
 
-        //Titulo
-        etiquetaTitulo.setFont(FUENTE_TITULO);
-        etiquetaTitulo.setVisible(true);
-        etiquetaTitulo.setBounds(400, 30, 400, 25);
-        panelContenido.add(etiquetaTitulo);
-
         //Formulario
         etiquetaId.setFont(FUENTE_ETIQUETAS);
         etiquetaId.setVisible(true);
-        etiquetaId.setBounds(150, 100, 200, 30);
+        etiquetaId.setBounds(150, 10, 200, 30);
         panelContenido.add(etiquetaId);
         campoId.setVisible(true);
-        campoId.setBounds(480, 100, 200, 30);
+        campoId.setBounds(300, 10, 200, 30);
         panelContenido.add(campoId);
         datos.addValue(100, "124563", "1/1/2020");
         datos.addValue(25, "124563", "1/5/2020");
         datos.addValue(36, "124563", "1/6/2020");
         botonGenerar.setFont(FUENTE_ETIQUETAS);
         botonGenerar.setVisible(true);
-        botonGenerar.setBounds(750, 100, 350, 30);
+        botonGenerar.setBounds(750, 10, 150, 30);
         botonGenerar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -94,9 +88,16 @@ public class GenerarReporteIndividual extends Container {
         
         panelContenido.add(botonGenerar);
 
+        botonLimpiar.setFont(FUENTE_ETIQUETAS);
+        botonLimpiar.setVisible(true);
+        botonLimpiar.setBounds(920, 10, 150, 30);
+        panelContenido.add(botonLimpiar);
+
         //Contenedor Reporte
         reporte.setVisible(true);
-        reporte.setBounds(0,150,1280,400);
+        reporte.setBounds(0,50,1280,525);
+        panelContenido.add(reporte);
+        this.add(panelContenido, BorderLayout.CENTER);
 
 
 
