6de0f66263ae10ebed8b9425ccb1200ec83a78d7
SebastianBetancourt <joan.betancourt@correounivalle.edu.co>
2020-12-02 12:22:37 -0500

    pruebas añadidas

--- src/control/Principal.java
+++ src/control/Principal.java
@@ -3,10 +3,14 @@ package src.control;
 import src.vista.Login;
 import src.vista.VentanaPrincipal;
 
+import src.managers.PruebasLib;
+
 public class Principal {
     public static void main(String[] args){
         Login login = new Login();
         login.setVisible(true);
+        // pruebas
+        //new PruebasLib();
     }
 
     public static void arrancarVentanaPrincipal(Usuario usuario){
