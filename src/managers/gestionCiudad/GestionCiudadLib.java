d2fa0f834b95d0ef42ce7012746ed06334df358c
Jorgemayor <jorge.e.mayor@gmail.com>
2020-12-02 07:51:06 -0500

    Refactorizada API ciudades

--- src/managers/gestionCiudad/GestionCiudadLib.java
+++ src/managers/gestionCiudad/GestionCiudadLib.java
@@ -12,18 +12,17 @@ import src.control.Conexion;
 public class GestionCiudadLib {
 
 	public String obtenerCiudades() throws SQLException {
-                Connection conexion = Conexion.conectar();
-                String consultaSQL = "SELECT * FROM ciudad";
-                PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
-                ResultSet respuesta = consulta.executeQuery();
-                JSONObject resultado = new JSONObject();
-                while(respuesta.next())
-                {
-                    resultado.append("id", respuesta.getString(1));
-                    resultado.append("nombre", respuesta.getString(2)); 
-                }
-                conexion.close();
-                return resultado.toString();
+        Connection conexion = Conexion.conectar();
+        String consultaSQL = "SELECT * FROM ciudad";
+        PreparedStatement consulta = conexion.prepareStatement(consultaSQL);
+        ResultSet respuesta = consulta.executeQuery();
+        JSONObject resultado = new JSONObject();
+        while(respuesta.next()) {
+            resultado.append("id", respuesta.getString(1));
+            resultado.append("nombre", respuesta.getString(2)); 
+        }
+        conexion.close();
+        return resultado.toString();
     }
     
     public String obtenerCiudadPorId(int id) throws SQLException {
@@ -35,6 +34,5 @@ public class GestionCiudadLib {
         respuesta.next();
         conexion.close();
         return respuesta.getString(2);
-}
-    
+    }   
 }
